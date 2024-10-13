c83d07285d444d20a7545c2e448ae8d08e9e4b27
shrish30mohan <131336106+shrish30mohan@users.noreply.github.com>
2024-10-13 17:30:15 +0530

    Update test_sum.py

--- test_sum.py
+++ test_sum.py
@@ -4,3 +4,6 @@ def test_add_numbers():
     assert add_numbers(2, 5, 10, 3) == 20
     assert add_numbers(1, 2) == 3
     assert add_numbers(0, 0, 0) == 0
+    print("assertions pass sucessfully")
+
+test_add_numbers()
